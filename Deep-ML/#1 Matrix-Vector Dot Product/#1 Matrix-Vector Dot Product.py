ee113bc3bbdea2cb1ccf4800f4af67b2b8cca558
Avinash <103890061+Theavinash02@users.noreply.github.com>
2025-05-08 21:32:50 +0530

    Update #1 Matrix-Vector Dot Product.py

--- Deep-ML/#1 Matrix-Vector Dot Product/#1 Matrix-Vector Dot Product.py
+++ Deep-ML/#1 Matrix-Vector Dot Product/#1 Matrix-Vector Dot Product.py
@@ -5,12 +5,10 @@ def matrix_dot_vector(
     ) -> list[int|float]:
     m = 0
 	# Return a list where each element is the dot product of a row of 'a' with 'b'.
-    for i in a:
-        m+=1
-    if m == len(b):
+    if len(a[0]) == len(b):  # comparing columns vs. vector length 
         mat = np.array(a) @ np.array(b)
         return mat.tolist()
+    # If the number of columns in 'a' does not match the length of 'b', return -1.
     else :
         return -1
-	# If the number of columns in 'a' does not match the length of 'b', return -1.
-	pass
+    pass
